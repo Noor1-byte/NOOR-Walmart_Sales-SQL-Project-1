93df1b63b74b6b1b24d7c8fbe5aa8a91df81a544
Noor1-byte <noorjee99127@gmail.com>
2025-05-18 07:30:20 +0530

    Update Walmart _Sales SQL PROJECT-1.sql

--- Walmart _Sales SQL PROJECT-1.sql
+++ Walmart _Sales SQL PROJECT-1.sql
@@ -74,14 +74,6 @@ ALTER TABLE WALMART_SALES ADD COLUMN time_of_day VARCHAR(20);
 
 SET SQL_SAFE_UPDATES = 0;
 
-UPDATE WALMART_SALES
-SET time_of_day = (
-	CASE
-		WHEN `time` BETWEEN "00:00:00" AND "12:00:00" THEN "Morning"
-        WHEN `time` BETWEEN "12:01:00" AND "16:00:00" THEN "Afternoon"
-        ELSE "Evening"
-    END
-);
 
 -- Add day_name column
 
